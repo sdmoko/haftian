71e8d15455922edad85f764d2e516f7f4accd9ee
Syah Dwi Prihatmoko <syahdwi92@gmail.com>
2016-07-31 19:29:50 +0700

    block system disk and use input

--- ngAftian.sh
+++ ngAftian.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-DEVICE=/dev/sdb
-
-parted  -s $DEVICE \
+echo "H. Aftian"
+echo "========="
+echo "Input your device"
+echo "============================================================"
+echo ""
+ROOT_MOUNTPOINT=`mount | grep ' on / ' | cut -d' ' -f1`
+ROOT=`/usr/bin/expr substr $ROOT_MOUNTPOINT 1 8`
+read -p "USBDISK: " devicevar
+if [ x$devicevar == x$ROOT ];then
+  echo "Error, it's your system partition"  
+  exit 1;
+fi
+parted  -s $devicevar \
   mktable gpt \
   -- mkpart efi fat32 1 -1 \
   toggle 1 boot
